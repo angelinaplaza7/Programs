135fb7f16fb4661cb00a204f35ba5f0010f121de
angelinaplaza7 <angiepj4@gmail.com>
2020-08-24 20:35:31 -0600

    Develop all components of Lab 1. Include update README.md, touch's necessary files, adds HelloWorld.

---
+++ HelloWorld/src/edu/nmsu/cs/HelloWorld/HelloWorld.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package edu.nmsu.cs.HelloWorld;
+
+/**
+ * @author nesqu
+ *
+ */
+public class HelloWorld {
+public static void main(String [] args)
+{
+	System.out.println("Hello world");
+	
+}
+}
